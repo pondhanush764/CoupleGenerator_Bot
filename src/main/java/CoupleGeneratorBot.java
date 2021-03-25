9ef818321428fe36dd942257759371aec5fbfc37
LuigiDN <gigidinardo2000@gmail.com>
2021-03-25 16:01:12 +0100

    Initial commit

---
+++ src/main/java/CoupleGeneratorBot.java
@@ -0,0 +1,33 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class CoupleGeneratorBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "sono io";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1752119241:AAEz0W5ZKaOs2oAMg4-lRY_9Yvtthh886o0";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
